a04ecc52c0f49c3652f9a5d4065484b035d61e12
duranurbinadanilonoe <duranurbinadanilonoe@gmail.com>
2014-11-16 16:27:53 -0500

    resumen02
    linea agregada para el control con git hub

--- MessageBoxIndirectCS/DemoForm.cs
+++ MessageBoxIndirectCS/DemoForm.cs
@@ -79,113 +79,114 @@ namespace MessageBoxIndirectApp
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.cmdPassLangID = new System.Windows.Forms.Button();
-			this.cmdCustomIcon = new System.Windows.Forms.Button();
-			this.statusBar1 = new System.Windows.Forms.StatusBar();
-			this.cmdAppModal = new System.Windows.Forms.Button();
-			this.cmdSysModal = new System.Windows.Forms.Button();
-			this.cmdTaskModal = new System.Windows.Forms.Button();
-			this.cmdHelpButton = new System.Windows.Forms.Button();
-			this.cmdHelpMsg = new System.Windows.Forms.Button();
-			this.cmdCustomIconAuxDll = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// cmdPassLangID
-			// 
-			this.cmdPassLangID.Location = new System.Drawing.Point(8, 8);
-			this.cmdPassLangID.Name = "cmdPassLangID";
-			this.cmdPassLangID.Size = new System.Drawing.Size(280, 24);
-			this.cmdPassLangID.TabIndex = 0;
-			this.cmdPassLangID.Text = "Pass a LangID";
-			this.cmdPassLangID.Click += new System.EventHandler(this.cmdPassLangID_Click);
-			// 
-			// cmdCustomIcon
-			// 
-			this.cmdCustomIcon.Location = new System.Drawing.Point(8, 32);
-			this.cmdCustomIcon.Name = "cmdCustomIcon";
-			this.cmdCustomIcon.Size = new System.Drawing.Size(280, 24);
-			this.cmdCustomIcon.TabIndex = 1;
-			this.cmdCustomIcon.Text = "Custom Icon (This Exe, Needs CmdLine Build)";
-			this.cmdCustomIcon.Click += new System.EventHandler(this.cmdCustomIcon_Click);
-			// 
-			// statusBar1
-			// 
-			this.statusBar1.Location = new System.Drawing.Point(0, 244);
-			this.statusBar1.Name = "statusBar1";
-			this.statusBar1.Size = new System.Drawing.Size(292, 22);
-			this.statusBar1.TabIndex = 2;
-			// 
-			// cmdAppModal
-			// 
-			this.cmdAppModal.Location = new System.Drawing.Point(8, 56);
-			this.cmdAppModal.Name = "cmdAppModal";
-			this.cmdAppModal.Size = new System.Drawing.Size(280, 24);
-			this.cmdAppModal.TabIndex = 3;
-			this.cmdAppModal.Text = "App Modal";
-			this.cmdAppModal.Click += new System.EventHandler(this.cmdAppModal_Click);
-			// 
-			// cmdSysModal
-			// 
-			this.cmdSysModal.Location = new System.Drawing.Point(8, 80);
-			this.cmdSysModal.Name = "cmdSysModal";
-			this.cmdSysModal.Size = new System.Drawing.Size(280, 24);
-			this.cmdSysModal.TabIndex = 4;
-			this.cmdSysModal.Text = "Sys Modal";
-			this.cmdSysModal.Click += new System.EventHandler(this.cmdSysModal_Click);
-			// 
-			// cmdTaskModal
-			// 
-			this.cmdTaskModal.Location = new System.Drawing.Point(8, 104);
-			this.cmdTaskModal.Name = "cmdTaskModal";
-			this.cmdTaskModal.Size = new System.Drawing.Size(280, 24);
-			this.cmdTaskModal.TabIndex = 5;
-			this.cmdTaskModal.Text = "Task Modal";
-			this.cmdTaskModal.Click += new System.EventHandler(this.cmdTaskModal_Click);
-			// 
-			// cmdHelpButton
-			// 
-			this.cmdHelpButton.Location = new System.Drawing.Point(8, 128);
-			this.cmdHelpButton.Name = "cmdHelpButton";
-			this.cmdHelpButton.Size = new System.Drawing.Size(280, 24);
-			this.cmdHelpButton.TabIndex = 6;
-			this.cmdHelpButton.Text = "Help Button (Callback)";
-			this.cmdHelpButton.Click += new System.EventHandler(this.cmdHelpButton_Click);
-			// 
-			// cmdHelpMsg
-			// 
-			this.cmdHelpMsg.Location = new System.Drawing.Point(8, 152);
-			this.cmdHelpMsg.Name = "cmdHelpMsg";
-			this.cmdHelpMsg.Size = new System.Drawing.Size(280, 24);
-			this.cmdHelpMsg.TabIndex = 7;
-			this.cmdHelpMsg.Text = "Help Button (WM_HELP)";
-			this.cmdHelpMsg.Click += new System.EventHandler(this.cmdHelpMsg_Click);
-			// 
-			// cmdCustomIconAuxDll
-			// 
-			this.cmdCustomIconAuxDll.Location = new System.Drawing.Point(8, 176);
-			this.cmdCustomIconAuxDll.Name = "cmdCustomIconAuxDll";
-			this.cmdCustomIconAuxDll.Size = new System.Drawing.Size(280, 24);
-			this.cmdCustomIconAuxDll.TabIndex = 8;
-			this.cmdCustomIconAuxDll.Text = "Custom Icon (Separate DLL)";
-			this.cmdCustomIconAuxDll.Click += new System.EventHandler(this.cmdCustomIconAuxDll_Click);
-			// 
-			// DemoForm
-			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.ClientSize = new System.Drawing.Size(292, 266);
-			this.Controls.Add(this.cmdCustomIconAuxDll);
-			this.Controls.Add(this.cmdHelpMsg);
-			this.Controls.Add(this.cmdHelpButton);
-			this.Controls.Add(this.cmdTaskModal);
-			this.Controls.Add(this.cmdSysModal);
-			this.Controls.Add(this.cmdAppModal);
-			this.Controls.Add(this.statusBar1);
-			this.Controls.Add(this.cmdCustomIcon);
-			this.Controls.Add(this.cmdPassLangID);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.Name = "DemoForm";
-			this.Text = "MessageBoxIndirect Demo";
-			this.ResumeLayout(false);
+            this.cmdPassLangID = new System.Windows.Forms.Button();
+            this.cmdCustomIcon = new System.Windows.Forms.Button();
+            this.statusBar1 = new System.Windows.Forms.StatusBar();
+            this.cmdAppModal = new System.Windows.Forms.Button();
+            this.cmdSysModal = new System.Windows.Forms.Button();
+            this.cmdTaskModal = new System.Windows.Forms.Button();
+            this.cmdHelpButton = new System.Windows.Forms.Button();
+            this.cmdHelpMsg = new System.Windows.Forms.Button();
+            this.cmdCustomIconAuxDll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdPassLangID
+            // 
+            this.cmdPassLangID.Location = new System.Drawing.Point(8, 8);
+            this.cmdPassLangID.Name = "cmdPassLangID";
+            this.cmdPassLangID.Size = new System.Drawing.Size(280, 24);
+            this.cmdPassLangID.TabIndex = 0;
+            this.cmdPassLangID.Text = "Pass a LangID";
+            this.cmdPassLangID.Click += new System.EventHandler(this.cmdPassLangID_Click);
+            // 
+            // cmdCustomIcon
+            // 
+            this.cmdCustomIcon.Location = new System.Drawing.Point(8, 32);
+            this.cmdCustomIcon.Name = "cmdCustomIcon";
+            this.cmdCustomIcon.Size = new System.Drawing.Size(280, 24);
+            this.cmdCustomIcon.TabIndex = 1;
+            this.cmdCustomIcon.Text = "Custom Icon (This Exe, Needs CmdLine Build)";
+            this.cmdCustomIcon.Click += new System.EventHandler(this.cmdCustomIcon_Click);
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.Location = new System.Drawing.Point(0, 244);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(292, 22);
+            this.statusBar1.TabIndex = 2;
+            // 
+            // cmdAppModal
+            // 
+            this.cmdAppModal.Location = new System.Drawing.Point(8, 56);
+            this.cmdAppModal.Name = "cmdAppModal";
+            this.cmdAppModal.Size = new System.Drawing.Size(280, 24);
+            this.cmdAppModal.TabIndex = 3;
+            this.cmdAppModal.Text = "App Modal";
+            this.cmdAppModal.Click += new System.EventHandler(this.cmdAppModal_Click);
+            // 
+            // cmdSysModal
+            // 
+            this.cmdSysModal.Location = new System.Drawing.Point(8, 80);
+            this.cmdSysModal.Name = "cmdSysModal";
+            this.cmdSysModal.Size = new System.Drawing.Size(280, 24);
+            this.cmdSysModal.TabIndex = 4;
+            this.cmdSysModal.Text = "Sys Modal";
+            this.cmdSysModal.Click += new System.EventHandler(this.cmdSysModal_Click);
+            // 
+            // cmdTaskModal
+            // 
+            this.cmdTaskModal.Location = new System.Drawing.Point(8, 104);
+            this.cmdTaskModal.Name = "cmdTaskModal";
+            this.cmdTaskModal.Size = new System.Drawing.Size(280, 24);
+            this.cmdTaskModal.TabIndex = 5;
+            this.cmdTaskModal.Text = "Task Modal";
+            this.cmdTaskModal.Click += new System.EventHandler(this.cmdTaskModal_Click);
+            // 
+            // cmdHelpButton
+            // 
+            this.cmdHelpButton.Location = new System.Drawing.Point(8, 128);
+            this.cmdHelpButton.Name = "cmdHelpButton";
+            this.cmdHelpButton.Size = new System.Drawing.Size(280, 24);
+            this.cmdHelpButton.TabIndex = 6;
+            this.cmdHelpButton.Text = "Help Button (Callback)";
+            this.cmdHelpButton.Click += new System.EventHandler(this.cmdHelpButton_Click);
+            // 
+            // cmdHelpMsg
+            // 
+            this.cmdHelpMsg.Location = new System.Drawing.Point(8, 152);
+            this.cmdHelpMsg.Name = "cmdHelpMsg";
+            this.cmdHelpMsg.Size = new System.Drawing.Size(280, 24);
+            this.cmdHelpMsg.TabIndex = 7;
+            this.cmdHelpMsg.Text = "Help Button (WM_HELP)";
+            this.cmdHelpMsg.Click += new System.EventHandler(this.cmdHelpMsg_Click);
+            // 
+            // cmdCustomIconAuxDll
+            // 
+            this.cmdCustomIconAuxDll.Location = new System.Drawing.Point(8, 176);
+            this.cmdCustomIconAuxDll.Name = "cmdCustomIconAuxDll";
+            this.cmdCustomIconAuxDll.Size = new System.Drawing.Size(280, 24);
+            this.cmdCustomIconAuxDll.TabIndex = 8;
+            this.cmdCustomIconAuxDll.Text = "Custom Icon (Separate DLL)";
+            this.cmdCustomIconAuxDll.Click += new System.EventHandler(this.cmdCustomIconAuxDll_Click);
+            // 
+            // DemoForm
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.cmdCustomIconAuxDll);
+            this.Controls.Add(this.cmdHelpMsg);
+            this.Controls.Add(this.cmdHelpButton);
+            this.Controls.Add(this.cmdTaskModal);
+            this.Controls.Add(this.cmdSysModal);
+            this.Controls.Add(this.cmdAppModal);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.cmdCustomIcon);
+            this.Controls.Add(this.cmdPassLangID);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "DemoForm";
+            this.Text = "MessageBoxIndirect Demo";
+            this.Load += new System.EventHandler(this.DemoForm_Load);
+            this.ResumeLayout(false);
 
 		}
 		#endregion
@@ -313,5 +314,10 @@ namespace MessageBoxIndirectApp
 
 			SetResult( mb.Show() );
 		}
+
+        private void DemoForm_Load(object sender, EventArgs e)
+        {
+            // linea de cambio para el control con github
+        }
 	}
 }
